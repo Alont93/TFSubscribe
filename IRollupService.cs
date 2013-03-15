0b315e698f4a4cd3572232fd5a50bebaec2fea79
Alont93 <alont93@hotmail.com>
2013-03-15 21:09:52 +0200

    first

---
+++ IRollupService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TFSubscriber
+{
+    [ServiceContract(Namespace="http://schemas.microsoft.com/TeamFoundation/2005/06/Services/Notification/03")]
+    public interface IRollupService
+    {
+ 
+        [OperationContract(Action = "http://schemas.microsoft.com/TeamFoundation/2005/06/Services/Notification/03/Notify")]
+        [XmlSerializerFormat(Style = OperationFormatStyle.Document)]
+        void Notify(string eventXml, string tfsIdentityXml);
+ 
+    }
+ 
+
+}
---
+++ RollupService.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml.Linq;
+
+namespace TFSubscriber
+{
+    public class RollupService : IRollupService
+    {
+        void IRollupService.Notify(string eventXml, string tfsIdentityXml)
+        {
+            global::System.Windows.Forms.MessageBox.Show("Test");
+            XDocument xml = XDocument.Parse(eventXml);
+            string path = xml.Root.Elements().First(e => e.Name == "Artifacts").Elements().ElementAt(1).Attributes("ServerItem").First().Value;
+
+
+            var dubug = 1;
+        }
+    }
+}
